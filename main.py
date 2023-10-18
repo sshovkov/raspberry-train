aefbac6e1cd0b19a81931b99270e13bfe0645c46
sshovkov <sophiashovkovy@gmail.com>
2023-10-17 22:27:14 -0400

    reverse order

--- main.py
+++ main.py
@@ -58,7 +58,12 @@ def calculate_minutes_until_next_arrival(sorted_arrivals):
 
 def display_minutes(minutes):
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=4, block_orientation=90, rotate=0)
+    device = max7219(
+        serial,
+        cascaded=4,
+        block_orientation=90,
+        blocks_arranged_in_reverse_order=True,
+    )
 
     message = f"{minutes} MIN"
 
