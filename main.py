fca6b1efe2593bb53f566a5a0e703cc15db7a949
sshovkov <sophiashovkovy@gmail.com>
2023-10-19 17:50:34 -0400

    better http response handling

--- main.py
+++ main.py
@@ -15,33 +15,62 @@ API_ENDPOINT = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgt
 STOP_ID = os.environ["STOP_ID"]
 feed = gtfs_realtime_pb2.FeedMessage()
 
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(
+    serial,
+    cascaded=4,
+    block_orientation=90,
+    rotate=0,
+    blocks_arranged_in_reverse_order=True,
+)
+
+MAX_RETRY_ATTEMPTS = 3
+
 
 def main():
     while True:
         minutes_remaining = get_train_schedule()
-        display_minutes(minutes_remaining)
+        if minutes_remaining:
+            display_minutes(minutes_remaining)
         time.sleep(60)
 
 
 def get_train_schedule():
-    response = requests.get(
-        API_ENDPOINT, headers={"x-api-key": os.environ["MTA_API_KEY"]}
-    )
-    feed_json = feed.FromString(response.content)
+    retry_attempt = 0
+    while retry_attempt < MAX_RETRY_ATTEMPTS:
+        try:
+            response = requests.get(
+                API_ENDPOINT, headers={"x-api-key": os.environ["MTA_API_KEY"]}
+            )
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            feed_json = feed.FromString(response.content)
 
-    stop_time_update_arrivals = [
-        stop_time_update
-        for entity in feed_json.entity
-        for stop_time_update in entity.trip_update.stop_time_update
-        if stop_time_update.stop_id == STOP_ID
-    ]
+            stop_time_update_arrivals = [
+                stop_time_update
+                for entity in feed_json.entity
+                for stop_time_update in entity.trip_update.stop_time_update
+                if stop_time_update.stop_id == STOP_ID
+            ]
 
-    # Sort the list from soonest to latest
-    sorted_arrivals = sorted(stop_time_update_arrivals, key=lambda x: x.arrival.time)
-    minutes_remaining = calculate_minutes_until_next_arrival(sorted_arrivals)
+            # Sort the list from soonest to latest
+            sorted_arrivals = sorted(
+                stop_time_update_arrivals, key=lambda x: x.arrival.time
+            )
+            minutes_remaining = calculate_minutes_until_next_arrival(sorted_arrivals)
 
-    # print(f"{minutes_remaining} MIN")
-    return minutes_remaining
+            # print(f"{minutes_remaining} MIN")
+            return minutes_remaining
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+            retry_attempt += 1
+            if retry_attempt < MAX_RETRY_ATTEMPTS:
+                time.sleep(10)
+            else:
+                print(f"Failed after max retry attempts.")
+                break
+
+    return None
 
 
 def calculate_minutes_until_next_arrival(sorted_arrivals):
@@ -59,17 +88,7 @@ def calculate_minutes_until_next_arrival(sorted_arrivals):
 
 
 def display_minutes(minutes):
-    serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(
-        serial,
-        cascaded=4,
-        block_orientation=90,
-        rotate=0,
-        blocks_arranged_in_reverse_order=True,
-    )
-
     message = f"{minutes} MIN"
-
     with canvas(device) as draw:
         text(draw, (3, 1), message, fill="white", font=proportional(LCD_FONT))
 
